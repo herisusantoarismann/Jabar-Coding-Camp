cb2c3c5705b85872ce8abd40042dc755d2ff0098
herisusantoarismann <heri.s.arisman@gmail.com>
2021-09-02 09:54:20 +0700

    Menambahkan jawaban tugas 4 - Nomor 2

--- Tugas-4-Javascript-Conditional-Dan-Loop/index.js
+++ Tugas-4-Javascript-Conditional-Dan-Loop/index.js
@@ -20,3 +20,51 @@ if (nilai > 100) {
 else {
   console.log("Nilai anda negatif. Mohon masukkan nilai antara 0-100 ");
 }
+
+// Soal Nomor 2
+var tanggal = 12;
+var bulan = 1;
+var tahun = 1999;
+
+// Jawaban Nomor 2
+switch (bulan) {
+  case 1:
+    console.log(tanggal + " Januari " + tahun);
+    break;
+  case 2:
+    console.log(tanggal + " Februari " + tahun);
+    break;
+  case 3:
+    console.log(tanggal + " Maret " + tahun);
+    break;
+  case 4:
+    console.log(tanggal + " April " + tahun);
+    break;
+  case 5:
+    console.log(tanggal + " Mei " + tahun);
+    break;
+  case 6:
+    console.log(tanggal + " Juni " + tahun);
+    break;
+  case 7:
+    console.log(tanggal + " Juli " + tahun);
+    break;
+  case 8:
+    console.log(tanggal + " Agustus " + tahun);
+    break;
+  case 9:
+    console.log(tanggal + " September " + tahun);
+    break;
+  case 10:
+    console.log(tanggal + " Oktober " + tahun);
+    break;
+  case 11:
+    console.log(tanggal + " November " + tahun);
+    break;
+  case 12:
+    console.log(tanggal + " Desember " + tahun);
+    break;
+  default:
+    console.log("Mohon masukkan bulan dengan benar");
+    break;
+}
