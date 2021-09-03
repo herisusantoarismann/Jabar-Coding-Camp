8f5924861b8d3049eca5b6402c12ea29215be8a3
herisusantoarismann <heri.s.arisman@gmail.com>
2021-09-03 09:21:26 +0700

    Menambahkan jawaban tugas 5 - Nomor 1

---
+++ Tugas-5-Javascript-Array-Function-Object/index.js
@@ -0,0 +1,8 @@
+// Soal Nomor 1
+var daftarHewan = ["2. Komodo", "5. Buaya", "3. Cicak", "4. Ular", "1. Tokek"];
+
+// Jawaban Nomor 1
+daftarHewan = daftarHewan.sort();
+for (let i = 0; i < daftarHewan.length; i++) {
+  console.log(daftarHewan[i]);
+}
