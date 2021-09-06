8cee9cdae75dd7ba1d814a1bc7a0c23042d6e163
herisusantoarismann <heri.s.arisman@gmail.com>
2021-09-06 08:31:12 +0700

    Menambahkan Jawaban Tugas 6 - Nomor 1

--- Tugas-6-ES6/index.js
+++ Tugas-6-ES6/index.js
@@ -1,4 +1,14 @@
-// Soal Nomor 1
+// Soal Nomor
+// Membuat arrrow Function
+// Jawaban Nomor 2
+const LuasPersegiPanjang = (p, l) => {
+  return p * l;
+};
+const KelilingPersegiPanjang = (p, l) => {
+  return 2(p + l);
+};
+
+// Soal Nomor 2
 // const newFunction = function literal(firstName, lastName) {
 //   return {
 //     firstName: firstName,
@@ -11,7 +21,7 @@
 
 // newFunction("William", "Imoh").fullName();
 
-// Jawaban Nomor 1
+// Jawaban Nomor 2
 const newFunction = (firstName, lastName) => {
   return {
     firstName: firstName,
